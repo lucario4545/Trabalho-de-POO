7c25f32abb95dae7bc39504e3a91cdd9013b9503
rlnascimento <rlnascimento@bip.b.br>
2016-10-21 21:27:22 -0200

    mudanças preliminares - precisa de teste

---
+++ trabalho/src/model/Carta.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Carta {
+	private String nome;
+	private int quantidade;
+	private String loja;
+	private double preco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getLoja() {
+		return loja;
+	}
+	public void setLoja(String loja) {
+		this.loja = loja;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+}
